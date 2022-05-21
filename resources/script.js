8f473ab9d35fe0109d0ad2066b2e8831aa81b4e1
kyahia <yahia.ah.kerdache@gmail.com>
2022-05-21 18:01:46 +0100

    v:3.0

--- resources/script.js
+++ resources/script.js
@@ -10,29 +10,44 @@ choices.forEach(element => {
         choices.forEach(choice => {
             choice.classList.remove('won');
             choice.classList.remove('lost');
-        })
+        });
+        computerDiv.classList.remove('won');
+        computerDiv.classList.remove('lost');
 
         // Play & Display
         score += game(e.target, e.target.id, computerPlay());
-        scoreSpan.textContent = score;
+        scoreSpan.textContent = ' ' + score;
     });
 });
 
 function computerPlay(){
+    const anime = [
+        { opacity: 0 },
+        { opacity: 1, offset: 0.25 },
+        { opacity: 0 }
+    ];
+    const delay = { duration: 2000, iterations: 1 };
+    
     let generated = ["Rock", "Paper", "Scissors"][Math.floor(Math.random()*3)];
+    
     computerDiv.textContent = `Computer choice is ${generated}`;
+    computerDiv.animate(anime, delay);
+
     return generated;
 }
 
 function game(element, player, computer){
     if (player == computer) {
+        computerDiv.style.color = 'black';
         return 0;
     }
     else if (player=="Rock" && computer=="Scissors" || player=="Paper" && computer=="Rock" || player=="Scissors" && computer=="Paper"){
+        computerDiv.classList.add('won');
         element.classList.add('won');
         return 1;
     }
     else {
+        computerDiv.classList.add('lost');
         element.classList.add('lost');
         return -1;
     }
